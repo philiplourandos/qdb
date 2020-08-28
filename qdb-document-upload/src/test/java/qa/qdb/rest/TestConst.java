cac4291baea6abcce99c3e2e33ae32dfe96c4e9c
Philip Lourandos <philip.lourandos@gmail.com>
2020-08-28 13:19:51 +0200

    #6
    * Need to re-use constants so I have moved them into a separate class

---
+++ qdb-document-upload/src/test/java/qa/qdb/rest/TestConst.java
@@ -0,0 +1,12 @@
+package qa.qdb.rest;
+
+public final class TestConst {
+    
+    public static final String SUBMITTER = "philip";
+    public static final String PARAM_SUBMITTER = "submitter";
+    public static final String ENDPOINT_DOCUMENT_UPLOAD_URL = "/document/upload";
+    public static final String ENDPOINT_GET_DOCUMENTS_URL = "/document/%s";
+    
+    private TestConst() {
+    }
+}
