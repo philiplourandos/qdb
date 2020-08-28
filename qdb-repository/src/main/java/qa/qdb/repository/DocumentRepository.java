20b6ff7583d3fe8722ae281ce4d21c9f42a6f0f6
Philip Lourandos <philip.lourandos@gmail.com>
2020-08-28 16:51:51 +0200

    #7
    * Added test cases for the removal of a document

--- qdb-repository/src/main/java/qa/qdb/repository/DocumentRepository.java
+++ qdb-repository/src/main/java/qa/qdb/repository/DocumentRepository.java
@@ -1,10 +1,13 @@
 package qa.qdb.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import qa.qdb.entities.Document;
 
 public interface DocumentRepository extends CrudRepository<Document, Long> {
 
-    public List<Document> findBySubmitter(String submitter);
+    List<Document> findBySubmitter(String submitter);
+
+    Optional<Document> findBySubmitterAndUuid(String submitter, String uuid);
 }
