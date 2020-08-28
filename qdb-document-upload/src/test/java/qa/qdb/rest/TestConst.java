ecc6f37150f0efb52ee55d22eec366f5d1793674
Philip Lourandos <philip.lourandos@gmail.com>
2020-08-28 16:52:25 +0200

    #7
    * Added constant for endpoint to delete document

--- qdb-document-upload/src/test/java/qa/qdb/rest/TestConst.java
+++ qdb-document-upload/src/test/java/qa/qdb/rest/TestConst.java
@@ -6,6 +6,7 @@ public final class TestConst {
     public static final String PARAM_SUBMITTER = "submitter";
     public static final String ENDPOINT_DOCUMENT_UPLOAD_URL = "/document/upload";
     public static final String ENDPOINT_GET_DOCUMENTS_URL = "/document/submitter/%s";
+    public static final String ENDPOINT_DELETE_DOCUMENT = "/document/gprs/%s/%s";
     
     private TestConst() {
     }
