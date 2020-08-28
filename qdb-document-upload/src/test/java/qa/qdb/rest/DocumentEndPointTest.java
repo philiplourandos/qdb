6ca2347827e1457b964f892cc3ab22ed53c3346b
Philip Lourandos <philip.lourandos@gmail.com>
2020-08-28 11:29:56 +0200

    #4
    * Added additional checks for success test. Picked an issue with the Blob object

--- qdb-document-upload/src/test/java/qa/qdb/rest/DocumentEndPointTest.java
+++ qdb-document-upload/src/test/java/qa/qdb/rest/DocumentEndPointTest.java
@@ -1,5 +1,8 @@
 package qa.qdb.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.regex.Pattern;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -12,10 +15,16 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import qa.qdb.entities.Document;
+import qa.qdb.repository.DocumentRepository;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import qa.qdb.model.UploadedDocument;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,23 +35,44 @@ public class DocumentEndPointTest {
     
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private DocumentRepository docRepo;
+
+    @Autowired
+    private ObjectMapper mapper;
     
     @Test
     public void givenAPdfForUpload_whenSubmitted_thenSuccessfullySaveDocument() throws Exception {
         //given
+        final String submitter = "philip";
         final Resource uploadResource = new ClassPathResource("documents/valid.pdf");
         final MockMultipartFile uploadFile = new MockMultipartFile("upload", 
                 uploadResource.getFilename(), MediaType.APPLICATION_PDF_VALUE,
                 uploadResource.getInputStream());
 
         //when
-        mvc.perform(multipart("/document/upload")
+        final MvcResult result = mvc.perform(multipart("/document/upload")
                 .file(uploadFile)
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .param("submitter", "philip"))
+                .param("submitter", submitter))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.uuid").isString())
-                .andExpect(jsonPath("$.uuid").value(Matchers.matchesRegex(UUID_REGEX)));
+                .andExpect(jsonPath("$.uuid").value(Matchers.matchesRegex(UUID_REGEX)))
+                .andReturn();
+
+        //then
+        final UploadedDocument reponse = mapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8),
+                UploadedDocument.class);
+        
+        final List<Document> savedDocs = docRepo.findBySubmitter(submitter);
+        assertNotNull(savedDocs);
+        assertEquals(1, savedDocs.size());
+
+        final Document savedDoc = savedDocs.get(0);
+        assertEquals(submitter, savedDoc.getSubmitter());
+        assertEquals(uploadResource.getFilename(), savedDoc.getFilename());
+        assertEquals(reponse.getUuid(), savedDoc.getUuid());
     }
 
     @Test
