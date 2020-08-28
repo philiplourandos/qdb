2b7d834344aa83e8840f40f3c49c0c30951be955
Philip Lourandos <philip.lourandos@gmail.com>
2020-08-28 18:27:40 +0200

    #8* Added entry point to application

---
+++ qdb-cms/src/main/java/qa/qdb/Application.java
@@ -0,0 +1,13 @@
+package qa.qdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Application {
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
