bc371e2386e6664594d0c6cc20708bb46da329c1
Philip Lourandos <philip.lourandos@gmail.com>
2020-08-29 14:16:57 +0200

    #13
    * Removed comments work

--- qdb-repository/src/main/java/qa/qdb/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package qa.qdb.repository;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-import qa.qdb.entities.Comment;
-
-public interface CommentRepository extends CrudRepository<Comment, Long> {
-    List<Comment> findByDocumentId(Long documentId);
-}
