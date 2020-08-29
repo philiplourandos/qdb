e66594399153d617930fb2fffce2b74fb100c396
Philip Lourandos <philip.lourandos@gmail.com>
2020-08-29 17:55:05 +0200

    #14
    * Added new constant for get post endpoint

--- qdb-cms/src/test/java/qa/qdb/cms/TestConst.java
+++ qdb-cms/src/test/java/qa/qdb/cms/TestConst.java
@@ -2,6 +2,7 @@ package qa.qdb.cms;
 
 public final class TestConst {
     public static final String ENDPOINT_CREATE_POST = "/cms/post/%s";
+    public static final String ENDPOINT_GET_POST = "/cms/post/%s";
     
     public static final Long VALID_SUBMITTER_ID = 5555L;
 
