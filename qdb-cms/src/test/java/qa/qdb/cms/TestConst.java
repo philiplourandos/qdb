1fe59e2eae984efe2b10e38677b527b30fce7ba9
Philip Lourandos <philip.lourandos@gmail.com>
2020-08-29 16:57:23 +0200

    #14
    * Added test constants file

---
+++ qdb-cms/src/test/java/qa/qdb/cms/TestConst.java
@@ -0,0 +1,10 @@
+package qa.qdb.cms;
+
+public final class TestConst {
+    public static final String ENDPOINT_CREATE_POST = "/cms/post/%s";
+    
+    public static final Long VALID_SUBMITTER_ID = 5555L;
+
+    private TestConst() {
+    }
+}
