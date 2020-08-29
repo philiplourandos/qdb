305b89c36e48b0acb0b92e7477fcc628b676849b
Philip Lourandos <philip.lourandos@gmail.com>
2020-08-29 16:56:31 +0200

    #14
    * Added test sql data

---
+++ qdb-cms/src/test/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,2 @@
+INSERT INTO DOCUMENT(submitter, filename, uuid, content) VALUES(5555, 'test.pdf', 'ef9f784f-a6cf-4d1f-b435-1ba73e529f0b', RAWTOHEX('test data'));
+INSERT INTO DOCUMENT(submitter, filename, uuid, content) VALUES(5555, 'test1.pdf', '7654a932-a4b1-43b0-8300-b9bdc48ca8f4', RAWTOHEX('more test data'));
